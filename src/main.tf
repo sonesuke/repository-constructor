235e7b7c97c338ca87f935066cddb0c52fab7223
sonesuke <iamsonesuke@gmail.com>
2026-02-28 10:53:44 +0900

    feat(terraform): add cypher-rs repository configuration

--- src/main.tf
+++ src/main.tf
@@ -32,6 +32,11 @@ locals {
       codeql_languages      = ["actions", "rust"]
       dependabot_ecosystems = ["cargo", "github-actions"]
     }
+    cypher-rs = {
+      manage_files          = true
+      codeql_languages      = ["actions", "rust"]
+      dependabot_ecosystems = ["cargo", "github-actions"]
+    }
   }
 
   # Default configuration
